46df4feb3bc7f6d2b1bae7576115598d7b964299
Michael Glass <me@mike.is>
2026-03-29 14:03:55 +0200

    feat: add SymbolStore/SymbolSink port types and adapter in Ports.fs

---
+++ src/TestPrune.Core/Ports.fs
@@ -0,0 +1,38 @@
+module TestPrune.Ports
+
+open TestPrune.AstAnalyzer
+open TestPrune.Database
+
+/// Port for reading symbol data from storage.
+type SymbolStore =
+    { GetSymbolsInFile: string -> SymbolInfo list
+      GetDependenciesFromFile: string -> Dependency list
+      GetTestMethodsInFile: string -> TestMethodInfo list
+      GetFileKey: string -> string option
+      GetProjectKey: string -> string option
+      QueryAffectedTests: string list -> TestMethodInfo list
+      GetAllSymbols: unit -> SymbolInfo list
+      GetAllSymbolNames: unit -> Set<string>
+      GetReachableSymbols: string list -> Set<string>
+      GetTestMethodSymbolNames: unit -> Set<string> }
+
+/// Port for writing symbol data to storage.
+type SymbolSink =
+    { RebuildProjects: AnalysisResult list -> (string * string) list -> (string * string) list -> unit }
+
+/// Create a SymbolStore from a Database instance.
+let toSymbolStore (db: Database) : SymbolStore =
+    { GetSymbolsInFile = db.GetSymbolsInFile
+      GetDependenciesFromFile = db.GetDependenciesFromFile
+      GetTestMethodsInFile = db.GetTestMethodsInFile
+      GetFileKey = db.GetFileKey
+      GetProjectKey = db.GetProjectKey
+      QueryAffectedTests = db.QueryAffectedTests
+      GetAllSymbols = db.GetAllSymbols
+      GetAllSymbolNames = fun () -> db.GetAllSymbolNames()
+      GetReachableSymbols = db.GetReachableSymbols
+      GetTestMethodSymbolNames = db.GetTestMethodSymbolNames }
+
+let toSymbolSink (db: Database) : SymbolSink =
+    { RebuildProjects =
+        fun results fileKeys projectKeys -> db.RebuildProjects(results, fileKeys = fileKeys, projectKeys = projectKeys) }
