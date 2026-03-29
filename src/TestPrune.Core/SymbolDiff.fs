957d7e544160ef69745559febad14d49e35ec7bb
Michael Glass <me@mike.is>
2026-03-29 10:23:52 +0200

    feat: SymbolDiff.detectChanges emits AnalysisEvents

--- src/TestPrune.Core/SymbolDiff.fs
+++ src/TestPrune.Core/SymbolDiff.fs
@@ -1,6 +1,7 @@
 module TestPrune.SymbolDiff
 
 open TestPrune.AstAnalyzer
+open TestPrune.Domain
 
 /// A change detected between stored and current symbols (Modified, Added, or Removed).
 type SymbolChange =
@@ -13,7 +14,10 @@ type SymbolChange =
 /// - It exists in both but content hash differs (Modified)
 /// - Only in current (Added)
 /// - Only in stored (Removed)
-let detectChanges (currentSymbols: SymbolInfo list) (storedSymbols: SymbolInfo list) : SymbolChange list =
+let detectChanges
+    (currentSymbols: SymbolInfo list)
+    (storedSymbols: SymbolInfo list)
+    : SymbolChange list * AnalysisEvent list =
     let currentByName =
         currentSymbols |> List.map (fun s -> s.FullName, s) |> Map.ofList
 
@@ -39,12 +43,29 @@ let detectChanges (currentSymbols: SymbolInfo list) (storedSymbols: SymbolInfo l
             else
                 None)
 
-    added @ removed @ modified
-    |> List.sortBy (fun change ->
-        match change with
-        | Added name
-        | Removed name
-        | Modified name -> name)
+    let changes =
+        added @ removed @ modified
+        |> List.sortBy (fun change ->
+            match change with
+            | Added name
+            | Removed name
+            | Modified name -> name)
+
+    let events =
+        changes
+        |> List.map (fun change ->
+            match change with
+            | Modified name ->
+                let sourceFile = currentByName[name].SourceFile
+                SymbolChangeDetectedEvent(sourceFile, name, "Modified")
+            | Added name ->
+                let sourceFile = currentByName[name].SourceFile
+                SymbolChangeDetectedEvent(sourceFile, name, "Added")
+            | Removed name ->
+                let sourceFile = storedByName[name].SourceFile
+                SymbolChangeDetectedEvent(sourceFile, name, "Removed"))
+
+    changes, events
 
 /// Extract just the symbol names from changes.
 let changedSymbolNames (changes: SymbolChange list) : string list =
