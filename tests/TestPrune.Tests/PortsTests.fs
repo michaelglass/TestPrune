d63cdb1085a9d75a13b51d47690cea27c395b411
Michael Glass <me@mike.is>
2026-03-29 14:03:55 +0200

    feat: add SymbolStore/SymbolSink port types and adapter in Ports.fs

---
+++ tests/TestPrune.Tests/PortsTests.fs
@@ -0,0 +1,60 @@
+module TestPrune.Tests.PortsTests
+
+open Xunit
+open Swensen.Unquote
+open TestPrune.AstAnalyzer
+open TestPrune.Database
+open TestPrune.Ports
+open TestPrune.Tests.TestHelpers
+
+module ``SymbolStore from Database`` =
+
+    [<Fact>]
+    let ``store wraps database GetSymbolsInFile`` () =
+        withDb (fun db ->
+            let graph =
+                { Symbols =
+                    [ { FullName = "Lib.func"
+                        Kind = Function
+                        SourceFile = "src/Lib.fs"
+                        LineStart = 1
+                        LineEnd = 5
+                        ContentHash = "abc" } ]
+                  Dependencies = []
+                  TestMethods = [] }
+
+            db.RebuildProjects([ graph ])
+            let store = toSymbolStore db
+            let symbols = store.GetSymbolsInFile "src/Lib.fs"
+            test <@ symbols.Length = 1 @>
+            test <@ symbols[0].FullName = "Lib.func" @>)
+
+    [<Fact>]
+    let ``store wraps database QueryAffectedTests`` () =
+        withDb (fun db ->
+            let store = toSymbolStore db
+            let tests = store.QueryAffectedTests [ "nonexistent" ]
+            test <@ tests |> List.isEmpty @>)
+
+module ``SymbolSink from Database`` =
+
+    [<Fact>]
+    let ``sink wraps database RebuildProjects`` () =
+        withDb (fun db ->
+            let sink = toSymbolSink db
+            let store = toSymbolStore db
+
+            let graph =
+                { Symbols =
+                    [ { FullName = "Lib.func"
+                        Kind = Function
+                        SourceFile = "src/Lib.fs"
+                        LineStart = 1
+                        LineEnd = 5
+                        ContentHash = "abc" } ]
+                  Dependencies = []
+                  TestMethods = [] }
+
+            sink.RebuildProjects [ graph ] [] []
+            let symbols = store.GetSymbolsInFile "src/Lib.fs"
+            test <@ symbols.Length = 1 @>)
